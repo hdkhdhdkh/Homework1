42f2a5b231f2e7e8f0c0323e595c210367c9b5b6
Ming Cheng <102703663+hdkhdhdkh@users.noreply.github.com>
2022-04-09 12:05:22 -0400

    getBondPrice Change 1
    write the method getBondPrice

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,6 +1,16 @@
 
 
 def getBondPrice(y, face, couponRate, m, ppy=1):
+ pvcfsum =0
+ cf = couponRate*face/ppy
+ for t in range(1,(m*ppy+1)):
+    pv =(1+y/ppy)**(-t)
+    pvcf = pv*cf
+    pvcfsum += pvcf
+    
+ bondprice = pvcfsum + pv*face
+ return(bondprice)
+    
     if ppy == 1:
         x = 2170604
     if ppy == 2:
