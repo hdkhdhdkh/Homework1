38ef141caaa6bdc2b77295139a1eef8338279bbf
Ming Cheng <102703663+hdkhdhdkh@users.noreply.github.com>
2022-04-09 17:05:52 -0400

    getBondPrice_E change 1
    write the method

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,5 +1,18 @@
 
-
 def getBondPrice_E(face, couponRate, m, yc):
-    x=3
-    return(2098949)
+ pvcfsum =0
+ cf = couponRate*face
+ for i,y in enumerate(yc):
+    pv =(1+y)**(-(i+1))
+    pvcf = pv*cf
+    pvcfsum += pvcf
+    
+ bondprice = pvcfsum + pv*face
+ return(bondprice)   
+
+
+yc = [.010,.015,.020,.025,.030]
+face = 2000000
+couponRate = .04
+m = 5
+getBondPrice_E(face, couponRate, m, yc)
