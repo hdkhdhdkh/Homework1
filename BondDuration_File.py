e0b6f45030025c585e119933f8b230dfda401e3f
Ming Cheng <102703663+hdkhdhdkh@users.noreply.github.com>
2022-04-09 15:55:45 -0400

    getBondDuration change 1
    write down the method

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,4 +1,23 @@
 
 def getBondDuration(y, face, couponRate, m, ppy = 1):
-    x=2
-    return(8.51)
+ pvcfsum =0 
+ cf=0
+ pvcf=0
+ t=1
+ wt=0  
+ while t<=m:
+    if t<m:
+        cf = face * couponRate
+    elif t == m:
+        cf = face + (face * couponRate)
+     
+    wt=wt+t*((1 + y)**(-t)) * cf
+    pvcfsum = pvcfsum + ((1 + y )**(-t)) * cf
+    t+=1
+    
+    #bondDuration = wt/pvcfsum
+
+ return(wt/pvcfsum)
+   
+    
+  
