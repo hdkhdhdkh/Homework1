2f6dea17adf4f8be605dfde90519cfd3a79e201f
Ming Cheng <102703663+hdkhdhdkh@users.noreply.github.com>
2022-04-09 23:41:02 -0400

    FizzBuzz change 1
    write the methos

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,5 +1,20 @@
 
 
 def FizzBuzz(start, finish):
-    v = ['buzz', 41, 'fizz', 43, 44, 'fizzbuzz']
-    return(v)
+    n= finish-start+1
+    outlist = []
+    for i in range(0,n):
+        index = start + i
+        if index % 3 == 0 and index % 5 == 0:
+            outlist.append("fizzbuzz")
+            continue
+        elif index % 3 == 0:
+            outlist.append("fizz")
+            continue
+        elif index % 5 == 0:
+            outlist.append("buzz")
+            continue
+        else:
+            outlist.append(index)
+
+    return(outlist)    
