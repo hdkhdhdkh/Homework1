f1dc72d62a058be0b08b9f0378c6f6372bdacc42
Ming Cheng <102703663+hdkhdhdkh@users.noreply.github.com>
2022-04-09 22:42:51 -0400

    BondPrice_Z change 1
    write the method

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -1,4 +1,18 @@
 
 
 def getBondPrice_Z(face, couponRate, times, yc):
-    return(1996533)
+ pvcfsum =0
+ cf = couponRate*face
+ for y,t in zip(yc,times):
+   pv =(1+y)**(-t)
+   pvcf = pv*cf
+   pvcfsum += pvcf
+
+ bondprice = pvcfsum + pv*face
+ return(bondprice) 
+
+yc = [.010,.015,.020,.025,.030]
+times=[1,1.5,3,4,7]
+face = 2000000
+couponRate = .04
+getBondPrice_Z(face, couponRate, times, yc)
